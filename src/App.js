b4c8621454e364f84509f4e74da27aaa232408c7
GameBak <gamebak@null.co>
2024-02-03 11:54:42 +0100

    Implementation of Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Navbar from "./components/Layouts/Navbar";
+import Footer from "./components/Layouts/Footer";
+
 function App() {
 	return (
-		<div className="App">
+		<Router>
+			<Navbar />
 			
-		</div>
+			<Routes>
+				<Route index path="/" element={<Home />} />
+
+				<Route path="*" element={<Navigate replace to="/" />} />
+			</Routes>
+
+			<Footer />
+		</Router>
 	);
 }
 
