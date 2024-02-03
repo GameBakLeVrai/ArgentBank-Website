88fc8c44a711571e0b1ae8afe228dae5118ff590
GameBak <gamebak@null.co>
2024-02-03 11:55:15 +0100

    Implementation of Footer and Navbar with css

---
+++ src/components/Layouts/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+import argentBankLogo from "../../images/argentBankLogo.png";
+import ProfileImg from "../../images/profileImg";
+
+const Navbar = () => {
+	return (
+		<nav className="main-nav">
+			<Link className="main-nav-logo" to="/">
+				<img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
+				<h1 className="sr-only">Argent Bank</h1>
+			</Link>
+
+			<div>
+				<Link className="main-nav-item" to="/sign-in">
+					<ProfileImg />
+					Sign In
+				</Link>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
