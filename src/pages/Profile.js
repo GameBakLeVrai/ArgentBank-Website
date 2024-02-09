a117387073d853fc404f3aea63907826215e1f59
GameBak <gamebak@null.co>
2024-02-09 12:28:06 +0100

    TransactionCard implementation

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
+import TransactionCard from '../components/transactionCard';
 
 const Profile = () => {
 	const dispatch = useDispatch();
 
 	const { user } = useSelector((state) => state.user);
-	
 	if(!user) return <div>Loading ...</div>;
 
 	return (
@@ -20,44 +20,10 @@ const Profile = () => {
 			</div>
 
 			<h2 className="sr-only">Accounts</h2>
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Checking (x8349)</h3>
 
-					<p className="account-amount">$2,082.79</p>
-					<p className="account-amount-description">Available Balance</p>
-				</div>
-
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
-
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Savings (x6712)</h3>
-
-					<p className="account-amount">$10,928.42</p>
-					<p className="account-amount-description">Available Balance</p>
-				</div>
-
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
-
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-
-					<p className="account-amount">$184.30</p>
-					<p className="account-amount-description">Current Balance</p>
-				</div>
-
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
+			<TransactionCard title={"Argent Bank Checking (x8349)"} amount={"2,082.79"} description={"Available Balance"} />
+			<TransactionCard title={"Argent Bank Checking (x6712)"} amount={"10,928.42"} description={"Available Balance"} />
+			<TransactionCard title={"Argent Bank Checking (x8349)"} amount={"184.30"} description={"Current Balance"} />
 		</main>
 	);
 };
